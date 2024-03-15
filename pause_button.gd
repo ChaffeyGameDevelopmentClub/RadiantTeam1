7380cd673e4d074d971f21d7711e18517e559220
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-03-14 22:30:53 -0700

    fixed import

---
+++ pause_button.gd
@@ -0,0 +1,34 @@
+extends Control
+@onready var main = $Menu
+@onready var Pause = $Pause_Button
+
+func resume():
+	get_tree().paused = false
+
+func pause():
+	get_tree().paused = true
+	
+func testEsc():
+	if Input.is_action_just_pressed("Pause") and get_tree().paused ==false:
+		pause()
+	elif Input.is_action_just_pressed("Pause") and get_tree().paused ==true:
+		resume()
+
+func _on_continue_pressed():
+	resume()
+	Engine.time_scale = 1 
+	
+func _on_back_to_tilte_screen_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_level_selection_pressed():
+	#For the phonebook \
+	get_tree().change_scene_to_file("res://level_selection.tscn")
+	
+func _process(_delta):
+		testEsc()
+
+
+func _on_quit_pressed():
+	get_tree().quit()
