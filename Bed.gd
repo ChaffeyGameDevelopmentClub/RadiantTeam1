cee0db2d00c6e75f7c715791e6673c2c3dd12e6d
lizzymak <lizmaks2@gmail.com>
2024-03-14 21:36:24 -0700

    s

--- Bed.gd
+++ Bed.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-@onready var text:TextEdit=$"../CanvasLayer/TextEdit"
+@onready var text:RichTextLabel=$"../CanvasLayer/TextEdit"
 @onready var bedTarget:Area2D=$"../BedArea"
 @onready var bedSprite:Sprite2D=$"../BedSprite"
 @onready var control:CharacterBody2D=$"."
@@ -13,13 +13,32 @@ const JUMP_VELOCITY = -400.0
 @onready var shelfCol:CollisionShape2D=$"../ShelfArea/CollisionShape2D"
 @onready var bedCol:CollisionShape2D=$"../BedArea/CollisionShape2D"
 @onready var girlIcon:Sprite2D=$"../BlueGirl"
-@onready var text2:TextEdit=$"../CanvasLayer/TextEdit2"
+@onready var text2:RichTextLabel=$"../CanvasLayer/TextEdit2"
 @onready var cat:Sprite2D=$"../cat"
 @onready var book:Sprite2D=$"../book"
 @onready var therapist:Sprite2D=$"../Therapist"
-
-
-
+@onready var finalShelf:Sprite2D=$"../finalShelf"
+@onready var button:TextureButton=$"../CanvasLayer/button"
+@onready var FINAL:ColorRect=$"../FINAL"
+@onready var lastButton:TextureButton=$"../FINAL/PhoneBook"
+var i=0
+var blueDialogue=["Hi doc. Ive been rotting\n in bed for like a week\n now all depressed.",
+"Rude. Anyways... I dont\nknow what to do",
+"Yeah i guess...",
+"Now what?",
+"What about my cat?",
+"Where should I put my\nHunterxHunter manga?",
+"My room feels more clean\nnow. I almost wanna be\nproductive now.",
+"Im too broke to pay\nyou so heres my HxH\nvolume 11."]
+var therapistDialogue=["I can tell","Maybe you would feel\nbetter if we cleaned\nthis messy room",
+"Maybe start by\nmoving your bed\nto the corner.",
+"Move your desk to\nmaybe not in the middle\n of the room",
+"Put her somewhere on\nthe bed",
+"I dont know what that is\nbut probaly on the\ndesk",
+"Well I hope you will be",
+"I still dont know what\nthat is but thank you"]
+func _ready():
+	set_physics_process(false)
 func _physics_process(delta):
 	if not Input.is_action_just_pressed("leftClick"):
 		var direction = Input.get_vector("left", "right","up","down")
@@ -35,39 +54,58 @@ func _physics_process(delta):
 func _on_bed_area_body_entered(body):
 	bedSprite.show()
 	bedTarget.hide()
-	shelfTarget.show()
-	control.position.x=150
-	control.position.y=180
+	shelfSprite.hide()
+	control.position.x=32
+	control.position.y=160
+	control.scale.x=1
+	control.scale.y=0.75
 	spriteChange.texture=load("res://Assets/Blue/desk.png")
-	text.text="Can you move \n my desk now"
-	
 	shelfCol.call_deferred("set","disabled",false)
+	button.show()
 
 func _on_shelf_area_body_entered(body):
-	shelfSprite.show()
 	shelfTarget.hide()
 	spriteChange.hide()
+	finalShelf.show()
+	button.show()
 	set_physics_process(false)
-	text.text="Please put my teddy \nbear somewhere \non the bed"
-	
-	
 
 func _input(event):
 	if event.is_pressed() and Input.is_action_just_pressed("leftClick"):
 		if event.position.x>1 and event.position.x<180 and event.position.y>10 and event.position.y<300:
 			cat.position.x=event.position.x/2.5
 			cat.position.y=event.position.y/2.6
-			cat.show()
-			text.text="Lastly can you \nput my book \non my desk"
+			button.show()
 		elif event.position.x>300 and event.position.x<530 and event.position.y>15 and event.position.y<90:
-			book.show()
 			book.position.x=event.position.x/2.5
 			book.position.y=event.position.y/2.5
-			finishGame()
+			button.show()
+			
 
 func finishGame():
-	text.text="thanks queen \nfor doing all dat"
-	therapist.show()
-	text2.show()
+	FINAL.show()
+	
+	
+func _on_texture_button_button_down():
+	i+=1
+	if i<8:
+		text.text=blueDialogue[i]
+		text2.text=therapistDialogue[i]
+	if i==2:
+		set_physics_process(true)
+		bedTarget.show()
+		button.hide()
+	elif i==3:
+		shelfTarget.show()
+		button.hide()
+	elif i==4:
+		button.hide()
+	elif i==5:
+		button.hide()
+	elif i==8:
+		button.hide()
+		finishGame()
 
 
+func _on_audio_stream_player_finished():
+	$"../BackGround/AudioStreamPlayer".play()
--- SkillCheck.gd
+++ SkillCheck.gd
@@ -3,23 +3,55 @@ extends Area2D
 @onready var punchingbag:Sprite2D=$matchup
 @onready var bar:ProgressBar=$ProgressBar
 @onready var effect:AnimatedSprite2D=$puncheffect
-@onready var redGuy2:Sprite2D=$"../RedGuy2"
-@onready var line1:TextEdit=$"../CanvasLayer/line1"
-@onready var line2:TextEdit=$"../CanvasLayer/line2"
+@onready var therapist:Sprite2D=$"../Therapist"
+@onready var redGuy:Sprite2D=$"../RedGuy"
+@onready var line1:RichTextLabel=$"../CanvasLayer/line1"
+@onready var line2:RichTextLabel=$"../CanvasLayer/line2"
+@onready var button:TextureButton=$"../CanvasLayer/textButton"
+@onready var FINAL:ColorRect=$"../FINAL"
 #var mspos:float=movingsprite.position.x
 #var pbpos:float=punchingbag.position.x
 var pos=2
-var speed=1
-
-
+var speed=1.5
+var i=0
+var dialogueRed=["Yeah you know, stop punching\nholes in the wall in the break\nroom, don't break the keyboards, stuff like that.",
+"Yeah doc, let's do it",
+"Sooo the gym, doc? I expected\nlike, some sort of hula by\nthe beachside dude",
+"Hey man this is pretty fun! Why\ndon't you give it a try",
+"Wow, man, this is really some\nfire stuff you've shown me doc,\nI feel like I could send a thousand emails right now!",
+"Here dude, I have a\nfeeling these things\nwill only slow me down."]
+var dialogueTherapist=["I see, I think I know what you mean. Why don't we see if we can work that out in a… healthier manner?",
+"Good",". . . Just punch the bag",
+"Thanks, but I think I'll pass on\nruining my knuckles",
+"Therapy is what works for\nyou… man. Im happy you found peace in throwing punches",
+"Thanks but please\ndon't break any\nmore electronics."]
+func _ready():
+	set_physics_process(false)
+	
+func _on_text_button_pressed():
+	if i<6:
+		line1.text=dialogueRed[i]
+		line2.text=dialogueTherapist[i]
+	if i==2:
+		set_physics_process(true)
+	elif i==6:
+		FINAL.show()
+	else:
+		i+=1
 func _physics_process(_delta):
 	movingsprite.position.x+=pos
 	if bar.value==10:
+		i=4
+		line1.text=dialogueRed[i]
+		line2.text=dialogueTherapist[i]
 		set_physics_process(false)
-		redGuy2.show()
-		line2.show()
-	if bar.value==5:
-		line1.text="Almost there!!"
+	if bar.value>4 and bar.value<9:
+		redGuy.texture=load("res://Assets/Red/red2.png")
+		i=3
+		line1.text=dialogueRed[3]
+		line2.text=dialogueTherapist[3]
+		
+		
 	if Input.is_action_just_pressed("space") and punchingbag.position.x<(movingsprite.position.x+40):
 		effectplace()
 		bagnewpos()
@@ -41,3 +73,6 @@ func effectplace():
 	effect.position.y=randi_range(80,180)
 	effect.show()
 	effect.play("effect")
+
+
+
